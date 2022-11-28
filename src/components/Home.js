462dd8e45f972d9bae451e508470e60708992898
1jiwoo27 <chloey1227@naver.com>
2022-11-28 21:58:17 +0900

    edit home

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,15 +4,17 @@ import homeanime from '../images/home_anime.gif'
 
 export default function Home() {
   return (
-    <div>
-    <div className='home-title'>
-      Computer Education 
-      Navigating Service
-    </div>
-    <img src={homeanime} alt="" class="animated"></img>
+    <>
+    <div className='home-middle'>
+      <div className='home-title'>
+        Computer Education 
+        Navigating Service
+      </div>
+      <img src={homeanime} alt="" className="animated"></img>
+      </div>
     <div className='copy-right'>
       Copyright @ CEPO All rights reserved
     </div>
-    </div>
+    </>
   )
 }
