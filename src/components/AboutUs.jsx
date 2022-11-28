fe64c9c901f558e8d9cba174b140302546a655f4
sjy2335 <106982330+sjy2335@users.noreply.github.com>
2022-11-28 20:53:45 +0900

    aboutus수정

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
 import '../css/AboutUs.css'
+import ms from '../images/ms.png'
+import jw from '../images/jw.png'
+import sj from '../images/sj.png'
+import hj from '../images/hj.png'
+
+
 
 export default function Aboutus(){
 
@@ -15,31 +21,31 @@ export default function Aboutus(){
                     <div className='ab-cardsframe'>
                         <div className='ab-card'>
 
-                            <img className='' src="" alt="없음" />
+                            <img className='ab-pic' src={ms} alt="없음" />
                             <div className='ab-name'>김민서</div>
-                            <div className='ab-role'>: Main Back-End Engineer</div>
+                            <div className='ab-role'>Main Back-End Engineer</div>
 
                         </div>
 
                         <div className='ab-card'>
-                            <img className='' src="" alt="없음" />
+                            <img className='ab-pic' src={jw} alt="없음" />
                             <div className='ab-name'>엄지우</div>
-                            <div className='ab-role'>: Main Front-End Engineer</div>
+                            <div className='ab-role'>Main Front-End Engineer</div>
                         </div>
 
                         <div className='ab-card'>
 
-                            <img className='' src="" alt="없음" />
+                            <img className='ab-pic' src={sj} alt="없음" />
                             <div className='ab-name'>윤상진</div>
-                            <div className='ab-role'>: Sub Front-End Engineer</div>
+                            <div className='ab-role'>Sub Front-End Engineer</div>
 
                         </div>
 
                         <div className='ab-card'>
 
-                            <img className='' src="" alt="없음" />
+                            <img className='ab-pic' src={hj} alt="없음" />
                             <div className='ab-name'>전혜진</div>
-                            <div className='ab-role'>: Sub Back-End Engineer</div>
+                            <div className='ab-role'>Sub Back-End Engineer</div>
 
                         </div>
                     </div>
